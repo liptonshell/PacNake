3572f9a7abf06b6cb38f811aeaf9aea2c06f8687
liptonshell <carlitos_260793@hotmail.com>
2015-05-05 14:13:57 +0100

    Game
    Just putted the game in the folder, start from the 0

---
+++ Pacnake/Game1.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pacnake
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+
+        clsTabuleiro Tab;
+        clsNake Pac;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferHeight = 630;//750
+            graphics.PreferredBackBufferWidth = 630;//700
+
+            IsMouseVisible = true;
+
+            Tab = new clsTabuleiro();
+            Pac=new clsNake();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            Pac.inicialize();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Tab.loadContent(Content);
+            Pac.loadContent(Content);
+        }
+
+
+        protected override void UnloadContent()
+        {
+            Tab.unloadContent();
+            Pac.unloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            Pac.update();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(Content.Load<Texture2D>("menu"), new Rectangle(0, 0, 630, 630), Color.White);
+            Tab.draw(spriteBatch);
+            Pac.draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Pacnake/clsNake.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Pacnake
+{
+    class clsNake
+    {
+        Texture2D Nake;
+        int pX1 = 1, pY1 = 1;
+        clsTabuleiro go;
+
+        public void inicialize()
+        {
+            go = new clsTabuleiro();
+        }
+
+        public void loadContent(ContentManager Content)
+        {
+            Nake = Content.Load<Texture2D>("Man1");
+        }
+
+        public void unloadContent()
+        {
+            Nake.Dispose();
+        }
+
+        public void update()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                if (go.CanGo(pX1, pY1 + 1))
+                {
+                    pY1++;
+                }
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                if (go.CanGo(pX1, pY1 - 1))
+                {
+                    pY1--;
+                }
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                if (go.CanGo(pX1 - 1, pY1))
+                {
+                    pX1--;
+                }
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                if (go.CanGo(pX1 + 1, pY1))
+                {
+                    pX1++;
+                }
+            }
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Nake, new Vector2(pX1 * 30, pY1 * 30), Color.White);
+        }
+    }
+}
